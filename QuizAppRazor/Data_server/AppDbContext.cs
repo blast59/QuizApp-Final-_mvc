eadc91297c3db0637fb407bc39c462f0bdce9a67
Durgaprasad <durgaprasad.m@secon.in>
2024-11-06 18:01:30 +0530

    done for the day(06 - 11 - 24)

--- QuizAppRazor/Data_server/AppDbContext.cs
+++ QuizAppRazor/Data_server/AppDbContext.cs
@@ -1,21 +1,30 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using QuizAppRazor.Models;
+using System.Configuration;
 
 namespace QuizAppRazor.Data_server
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
         protected readonly IConfiguration Configuration;
         public AppDbContext(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-       
-        public DbSet<Quiz> Quiz { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+            // connect to postgres with connection string from app settings
+            options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        public DbSet<Quiz> Quizzes { get; set; } 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
-        }
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Quiz>().HasData(
+                new Quiz { quiz_id = 1, Topic = "SQL" }
+                );
+        }   
     }
 }
